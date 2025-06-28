8ec11b6400dae02481062e0ccf0c24e6650042f5
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-28 21:02:26 +0600

    Exploring node

---
+++ creatingANode.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int integerValue;
+    Node* nextPointer;
+};
+
+int main()
+{
+    Node firstNode, secondNode, thirdNode;
+    return 0;
+}
