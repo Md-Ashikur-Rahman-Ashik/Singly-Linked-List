d55dfac4c0be74d420d0b1d7fcbfe5ad310228e7
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-28 20:43:06 +0600

    Exploring limitation of array

---
+++ linkedList.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int integerArray[5] = {1, 2, 3, 4, 5};
+    cout << &integerArray[2] << " " << &integerArray[3];
+
+    return 0;
+}
