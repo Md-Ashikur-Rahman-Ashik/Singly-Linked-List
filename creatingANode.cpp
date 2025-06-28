483350cd2e8231fd7e3a2625245d8fbb8a5b325f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-28 22:56:43 +0600

    Explored node creation

--- creatingANode.cpp
+++ creatingANode.cpp
@@ -1,14 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     int integerValue;
-    Node* nextPointer;
+    Node *nextPointer;
 };
 
 int main()
 {
     Node firstNode, secondNode, thirdNode;
+    firstNode.integerValue = 10;
+    secondNode.integerValue = 20;
+    thirdNode.integerValue = 30;
+
+    firstNode.nextPointer = &secondNode;
+    secondNode.nextPointer = &thirdNode;
+    thirdNode.nextPointer = NULL;
+
+    // cout << firstNode.integerValue << " " << secondNode.integerValue << " " << thirdNode.integerValue << endl;
+    cout << firstNode.integerValue << endl;
+    cout << firstNode.nextPointer->integerValue << endl;
     return 0;
 }
