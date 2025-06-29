718849582658b058604e98cf9dcf7a0c1aeb73f9
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-29 14:04:59 +0600

    Explored dynamic node

--- dynamicNode.cpp
+++ dynamicNode.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node
+{
+public:
+    int integerValue;
+    Node *nextPointer;
+
+    Node(int integerValue)
+    {
+        this->integerValue = integerValue;
+        this->nextPointer = NULL;
+    }
+};
+
 int main()
 {
+    Node *head = new Node(10);
+    Node *secondNode = new Node(20);
+    Node *thirdNode = new Node(30);
+
+    head->nextPointer = secondNode;
+    secondNode->nextPointer = thirdNode;
+
+    cout << head->integerValue << endl;
+    cout << head->nextPointer->integerValue << endl;
+
     return 0;
 }
